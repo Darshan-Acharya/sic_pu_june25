8181136c4040a9a73f5d800151f7cf2734c26da1
darshan <darshanacharya820@gmail.com>
2025-06-26 11:28:50 +0530

    updating

--- week1/assignment1_1.py
+++ week1/assignment1_1.py
@@ -2,6 +2,7 @@
 number = input("Enter the Number: ")
 my_list = []
 for i in number:
-    my_list.append[int(i)]
+    num = int(i)
+    my_list.append[num]
     my_list.sort()
-print("The second smallest number is ",my_list[1])
+print("The second smallest number is : ", my_list[1])
