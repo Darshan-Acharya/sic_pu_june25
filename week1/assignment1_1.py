bb54995f42438eb22c7a0cb165168ae6e30fd183
darshan <darshanacharya820@gmail.com>
2025-06-26 11:25:34 +0530

    assignment problem updating

---
+++ week1/assignment1_1.py
@@ -0,0 +1,7 @@
+#Find 2nd smallest digit in a number
+number = input("Enter the Number: ")
+my_list = []
+for i in number:
+    my_list.append[int(i)]
+    my_list.sort()
+print("The second smallest number is ",my_list[1])
--- week1/p1.py
+++ week1/p1.py
@@ -1,3 +1,13 @@
-average_score = int(input("Enter the student average score: "))
-if average_score >= 0 and average_score <= 69:
-    
+#assaignment questions
+#1.  Find biggest digit in a number
+number = input("Enter a number: ")
+big_number = 0
+for i in range(1, len(number)):
+    for j in range(i):
+        big_number = number[j]
+        if int(number[i]) > int(big_number):
+            big_number = int(number[i])
+        else:
+            big_number = int(number[j])
+    print(big_number)
+print(big_number, "is the biggest digit in the Number")
