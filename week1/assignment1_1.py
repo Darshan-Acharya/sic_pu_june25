0cc6ff25611444836ad8be1e6d2a405f80fefd70
darshan <darshanacharya820@gmail.com>
2025-06-26 11:42:44 +0530

    updating....

--- week1/assignment1_1.py
+++ week1/assignment1_1.py
@@ -1,3 +1,12 @@
+#assaignment questions
+#1.  Find biggest digit in a number
+number = input("Enter the Number: ")
+my_list = []
+for i in number:
+    num = int(i)
+    my_list.append(num)
+    my_list.sort()
+print("The largest number is : ", my_list[len(my_list)-1])
 #Find 2nd smallest digit in a number
 number = input("Enter the Number: ")
 my_list = []
@@ -6,3 +15,16 @@ for i in number:
     my_list.append(num)
     my_list.sort()
 print("The second smallest number is : ", my_list[1])
+#Count number of Prime digits in a number
+number = input("Enter the Number: ")
+count = 0
+for i in number:
+    num = int(i)
+    if num in [2, 3, 5, 7]:
+        count+=1
+print(count)
+#Print the Prime numbers in decreasing order between m and n (m < n)
+m = int(input("Enter the lower range: "))
+n = int(input("Enter the Higher range: "))
+for i in range(n, m, -1):
+    
