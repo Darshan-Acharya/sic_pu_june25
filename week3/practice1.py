e5b371320b9111276b907a469df6619e52379ab4
darshan <darshanacharya820@gmail.com>
2025-07-09 14:00:40 +0530

    UPDATED

--- week3/practice1.py
+++ week3/practice1.py
@@ -1,23 +1,33 @@
-# import numpy as np
-
-# arr1 = np.arange(12)
-
-# arr2 = arr1.reshape(2, 6)
-# arr3 = arr1.reshape(6, 2)
-# arr4 = arr1.reshape(3, 4)
-# arr5 = arr1.reshape(12, 1)
-# arr6 = arr1.reshape(4, 3)
-
-# print('Arr1:\n', arr1)
-# print('Arr2:\n', arr2)
-# print('Arr3:\n', arr3)
-# print('Arr4:\n', arr4)
-# print('Arr5:\n', arr5)
-# print('Arr6:\n', arr6)
-
-import pdb
-pdb.set_trace()
-import numpy as np
-
-arr = np.arange(1, 10).reshape(3, -3) # Here python infers/decides the number of columns by itself.
-print(arr)
+class Node:
+    def __init__(self, data = 0):
+        if data == 0:
+            self.left = None
+            self.data = int(input("Enter the data: "))
+            self.right = None
+class BST:
+    def __init__(self):
+        self.root = None
+        print("Empty Node is Created..!")
+    
+    def add_node(self):
+        new_node = Node()
+        if self.root == Node:
+            self.root = new_node
+            return
+        
+        else:
+            temp1 = new_node
+            temp2 = None
+            while temp1 != None:
+                temp2 = temp1
+                if new_node.data < temp1.data:
+                    temp1 = temp1.left
+                else:
+                    temp1 = temp1.right
+            if new_node.data < temp2.data:
+                temp2.left = new_node
+            else:
+                temp2.right = new_node
+    
+    def search_tree(self, temp, search_data):
+        if 
---
+++ week3/practice2.py
@@ -0,0 +1,4 @@
+import numpy as np
+array1 = np.arange(1, 10)
+print(array1)
+print(array1)
