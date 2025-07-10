8284640b01fa9c755e75d18f54e477340bab4f9b
darshan <darshanacharya820@gmail.com>
2025-07-10 15:59:23 +0530

    UPDATED

--- week3/florist.py
+++ week3/florist.py
@@ -1,16 +1,25 @@
-def get_minimum_cost(k, c):
-    c.sort()(reverse = True)
-    i = k
+import pdb as p
+p.set_trace()
+def getMinimumCost(k, c): # k = 2
+    c.sort() # 2 5 6
+    i = 1
+    j = 1
     minimum_cost = 0
-    while i > 0:
-        
+    while i <= len(c): # 1 2 3
+        if i <= k:
+            minimum_cost += c[-i] # 6 + 5
+        else:
+            minimum_cost += (j + 1) * c[-i]
+            if i % k == 0:
+                j += 1
+        i += 1
+    return minimum_cost
 
-
-
-
-nk = input("Enter the values for N and K: ").split()
-n = int(nk[0])
-k = int(nk[1])
-flowers_price = list(map(int, input("Enter the costs of flowers: ").split().strip()))
-
-print("")
+if __name__ == '__main__':
+    nk = input('Enter space seperated values for N and K: ').split()
+    n = int(nk[0])
+    k = int(nk[1])
+    print('Enter cost of the flowers:')
+    c = list(map(int, input().rstrip().split()))
+    minimumCost = getMinimumCost(k, c)
+    print(f'Minimum Cost = {minimumCost}')
