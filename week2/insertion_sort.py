5e0b04823e53769cdbca10a60c87d72239cb753a
Darshan-Acharya <darshanacharya820@gmail.com>
2025-07-13 22:17:48 +0530

    Update insertion_sort.py

--- week2/insertion_sort.py
+++ week2/insertion_sort.py
@@ -1,3 +1,15 @@
 #bubble sort optimization
 #max number of comparition of unoptimized bubble sort and optimized bubble sort
-#
+def insertion_sort(mylist):
+    length = len(mylist)
+    for i in range(1, length):
+        element = mylist [length]
+        position = i - 1
+        while position >= 0 and mylist[position] > elements:
+            mylist[position + 1] = mylist[position]
+    return mylist 
+mylist = list(map(int, input().strip().split()))
+insertion_sort(mylist)
+
+
+
