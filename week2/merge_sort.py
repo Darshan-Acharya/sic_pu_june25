2325e65f57a6dd825a46e3658db0722f79b8e622
Darshan-Acharya <darshanacharya820@gmail.com>
2025-07-15 20:10:12 +0530

    Update merge_sort.py

--- week2/merge_sort.py
+++ week2/merge_sort.py
@@ -1,2 +1,25 @@
 def merge_sort(array):
-    sorted_array = array[0]
+    length = len(array)
+    if length  > 1:
+        mid = length //2
+        left, right = [:mid:], [mid::]
+        merge_sort(left)
+        merge_sort(right)
+        merge_sort_in(array, left, right)
+def merge_sort_in(array, left, right):
+    index = 0
+    while len(left)> 1 and len(right)> 1:
+        if left[0]<= right[0]:
+            array[index] = left.pop(0)
+            index += 1
+        else:
+            array[index] = right.pop(0)
+            index += 1
+        while len(left) != 0:
+            array[index] = left.pop(0)
+            index += 1
+        while len(right) != 0:
+            array[index] = right.pop(0)
+            index += 1
+array = list(map(int, input("Enter the space separed array elements (unsorted): ").strip(). split ()))
+merge_sort(array)
