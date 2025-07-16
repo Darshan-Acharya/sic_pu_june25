903360af77e4c2948c4c4a79db83d58cb197dbba
darshan <darshanacharya820@gmail.com>
2025-07-16 16:11:59 +0530

    UPDATED

--- week3/practice5.py
+++ week3/practice5.py
@@ -1,10 +1,10 @@
-'''import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
+from google.colab import drive
 data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Age': [25, 30, 35],
         'Salary': [50000, 60000, 70000]}
 df = pd.DataFrame(data)
-plt.pie()
 print(df)
 df.to_csv('output.csv', index=False)
 df = pd.read_csv('output.csv')  # Reads the CSV file
@@ -12,14 +12,4 @@ df.head()  # Displays the first 5 rows
 print(df.isnull().sum())  # Counts missing values in each column
 grouped = df.groupby('Age')['Salary'].mean() 
 # Groups by Age and calculates mean salary
-print(grouped)'''
-import matplotlib.pyplot as plt
-
-sizes = [30, 20, 25, 25]
-labels = ['A', 'B', 'C', 'D']
-colors = ['blue', 'red', 'green', 'purple']
-
-# Create pie chart
-plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-plt.title("Pie Chart")
-plt.show()
+print(grouped)
